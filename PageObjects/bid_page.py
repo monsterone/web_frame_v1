9da7041bceeedfe025e4255bea70d394e2d797f6
monster <945534456@qq.com>
2020-06-02 01:34:58 +0800

    BasePage添加

--- PageObjects/bid_page.py
+++ PageObjects/bid_page.py
@@ -5,9 +5,9 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
-
+from Common.basepage import BasePage
 from PageLocators.bidpage_locators import BidPageLocator as BL
-class BidPage:
+class BidPage(BasePage):
 
     def __init__(self,driver):
         self.driver=driver
@@ -15,18 +15,18 @@ class BidPage:
     #投资-按钮点的动
     def invest(self,mount):
         #在输入框当中，输入金额
-        WebDriverWait(self.driver, 10).until(
-            EC.visibility_of_element_located(BL.invest_input))
-        self.driver.find_element(*BL.invest_input).send_keys(mount)
+        doc = "投资页面_投资操作"
+        self.wait_eleVisible(BL.invest_input,doc=doc)
+        self.input_text(BL.invest_input,mount,doc)
         #点击投标按钮
-        self.driver.find_element(*BL.do_invest_button).click()
+        self.click_element(BL.do_invest_button,doc)
 
     # 投资-按钮点不动
     def invest_no_click(self,mount):
-        #在输入框当中，输入金额
-        WebDriverWait(self.driver, 10).until(
-            EC.visibility_of_element_located(BL.invest_input))
-        self.driver.find_element(*BL.invest_input).send_keys(mount)
+        # 在输入框当中，输入金额
+        doc = "投资页面_投资操作"
+        self.wait_eleVisible(BL.invest_input, doc=doc)
+        self.input_text(BL.invest_input, mount, doc)
 
 
     #获取用户余额(输入框)
--- PageObjects/login_page.py
+++ PageObjects/login_page.py
@@ -1,15 +1,9 @@
 
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
+
 from PageLocators.loginpage_locators import LoginPageLocator as loc
 #引入页面元素定位页面，页可以继承该类，但元素多了不好区分，长远考虑用loc快速找到定位元素
-
-class LoginPage:
-
-
-    def __init__(self,driver):
-        self.driver = driver
+from Common.basepage import BasePage
+class LoginPage(BasePage):
 
 
     #登录操作
@@ -17,24 +11,27 @@ class LoginPage:
         #输入用户名
         #输入密码
         #点击登录
-        WebDriverWait(self.driver,20).until(EC.visibility_of_element_located(loc.name_text))
-        self.driver.find_element(*loc.name_text).send_keys(username)
-        self.driver.find_element(*loc.pwd_text).send_keys(passwd)
+        doc = "登录页面_登录功能"
+        self.wait_eleVisible(loc.name_text,doc=doc)
+        self.input_text(loc.name_text,username,doc)
+        self.input_text(loc.pwd_text,passwd,doc)
         #判断rember_user 的值，来决定，是否勾选
+        self.click_element(loc.login_but,doc)
         self.driver.find_element(*loc.login_but).click()
 
 
 
     #获取错误提示信息 - 登录区域
     def get_errorMsg_from_loginArea(self):
-        WebDriverWait(self.driver,20).until(EC.visibility_of_element_located(loc.errorMsg_from_loginArea))
-        return self.driver.find_element(*loc.errorMsg_from_loginArea).text
+        doc = "登录页面_获取错误提示信息(登录区域)"
+        self.wait_eleVisible(loc.errorMsg_from_loginArea, doc=doc)
+        return self.get_text(loc.errorMsg_from_loginArea,doc)
 
     # 获取错误提示信息 - 登录页面正中间
     def get_errorMsg_from_pageCenter(self):
-        WebDriverWait(self.driver, 20).until(
-            EC.visibility_of_element_located(loc.errorMsg_from_pageCenter))
-        return self.driver.find_element(*loc.errorMsg_from_pageCenter).text
+        doc = "登录页面_获取错误提示信息(登录页面正中间)"
+        self.wait_eleVisible(loc.errorMsg_from_pageCenter, doc=doc)
+        return self.get_text(loc.errorMsg_from_pageCenter, doc)
 
 
 
