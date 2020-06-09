f930c825382ded4b56be4018392cd20984ee4636
monster <945534456@qq.com>
2020-06-09 22:31:53 +0800

    invest用例

--- run_pytest.py
+++ run_pytest.py
@@ -3,7 +3,7 @@
 import pytest
 if __name__ == '__main__':
     # command_line = ["-s", "./tests/test1/test1.py", "--alluredir=report"]
-    command_line = ["-m","demo","--reruns","2","--reruns-delay","5","-s","--alluredir=Outputs/allure_reports"]
+    command_line = ["-m","invest","--reruns","2","--reruns-delay","5","-s","--alluredir=Outputs/allure_reports"]
     pytest.main(command_line)
 
 
